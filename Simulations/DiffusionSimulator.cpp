aec0eb4c54663686be163a2a53ef806d73f24015
Philipp <22404008+ste-phil@users.noreply.github.com>
2022-12-10 17:26:38 +0100

    Added explicit temperature simulation

--- Simulations/DiffusionSimulator.cpp
+++ Simulations/DiffusionSimulator.cpp
@@ -2,38 +2,45 @@
 #include "pcgsolver.h"
 using namespace std;
 
-Grid::Grid() {
-}
-
-
-DiffusionSimulator::DiffusionSimulator()
+DiffusionSimulator::DiffusionSimulator() 
+	: T(_gridSizeX, _gridSizeY)
 {
 	m_iTestCase = 0;
 	m_vfMovableObjectPos = Vec3();
 	m_vfMovableObjectFinalPos = Vec3();
 	m_vfRotate = Vec3();
-	// to be implemented
 }
+
+DiffusionSimulator::~DiffusionSimulator() { }
 
 const char * DiffusionSimulator::getTestCasesStr(){
 	return "Explicit_solver, Implicit_solver";
 }
 
-void DiffusionSimulator::reset(){
-		m_mouse.x = m_mouse.y = 0;
-		m_trackmouse.x = m_trackmouse.y = 0;
-		m_oldtrackmouse.x = m_oldtrackmouse.y = 0;
+void DiffusionSimulator::reset() {
+	m_mouse.x = m_mouse.y = 0;
+	m_trackmouse.x = m_trackmouse.y = 0;
+	m_oldtrackmouse.x = m_oldtrackmouse.y = 0;
 
+	this->InitializeTemperatureGrid();
 }
+
 
 void DiffusionSimulator::initUI(DrawingUtilitiesClass * DUC)
 {
 	this->DUC = DUC;
 	// to be implemented
+
+	TwAddVarRW(DUC->g_pTweakBar, "Alpha", TW_TYPE_FLOAT, &_alpha, "min=0.0,max=1.0");
+
+	TwAddVarRW(DUC->g_pTweakBar, "Size x", TW_TYPE_UINT32, &_gridSizeX, "");
+	TwAddVarRW(DUC->g_pTweakBar, "Size y", TW_TYPE_UINT32, &_gridSizeY, "");
 }
 
 void DiffusionSimulator::notifyCaseChanged(int testCase)
 {
+	reset();
+
 	m_iTestCase = testCase;
 	m_vfMovableObjectPos = Vec3(0, 0, 0);
 	m_vfRotate = Vec3(0, 0, 0);
@@ -54,10 +61,76 @@ void DiffusionSimulator::notifyCaseChanged(int testCase)
 	}
 }
 
-Grid* DiffusionSimulator::diffuseTemperatureExplicit() {//add your own parameters
-	Grid* newT = new Grid();
+void DiffusionSimulator::ProcessInput() {
+	if (_gridSizeX != T.GetSizeX() || _gridSizeY != T.GetSizeY()) {
+		T = Grid<double>(_gridSizeX, _gridSizeY);
+		this->InitializeTemperatureGrid();
+	}
+}
+
+void DiffusionSimulator::InitializeTemperatureGrid() {
+	uint32_t sizeX = T.GetSizeX();
+	uint32_t sizeY = T.GetSizeY();
+
+	for (uint32_t y = 0; y < sizeY; y++) 
+	{
+		for (uint32_t x = 0; x < sizeX; x++) 
+		{
+			auto isEdge = (x == 0 || x + 1 == sizeX) || (y == 0 || y + 1 == sizeY);
+			auto value = isEdge ? 0. : -1.;
+
+			T.SetValue(x, y, value);
+		}
+	}
+
+	T.SetValue(sizeX / 2, sizeY / 2, 200.);
+}
+
+void DiffusionSimulator::simulateTimestep(float timeStep)
+{
+	ProcessInput();
+
+	// to be implemented
+	// update current setup for each frame
+	switch (m_iTestCase)
+	{
+	case 0:
+		T = diffuseTemperatureExplicit(timeStep);
+		break;
+	case 1:
+		diffuseTemperatureImplicit();
+		break;
+	}
+}
+
+Grid<double> DiffusionSimulator::diffuseTemperatureExplicit(float timestep) {//add your own parameters
+	auto newT = Grid<double>(T.GetSizeX(), T.GetSizeY());
 	// to be implemented
 	//make sure that the temperature in boundary cells stays zero
+	
+	for (uint32_t y = 1; y < newT.GetSizeY() - 1; y++)
+	{
+		for (uint32_t x = 1; x < newT.GetSizeX() - 1; x++)
+		{
+			auto tVal = T.GetValue(x, y);
+			
+			auto tValNextX = T.GetValue(x+1, y);
+			auto tValPrevX = T.GetValue(x-1, y);
+							  
+			auto tValNextY = T.GetValue(x, y+1);
+			auto tValPrevY = T.GetValue(x, y-1);
+
+			auto alphaMul = _alpha * (
+				(-2. * tVal + tValNextX + tValPrevX) +
+				(-2. * tVal + tValNextY + tValPrevY)
+			);
+
+			auto val = tVal + alphaMul * timestep;
+			newT.SetValue(x, y, val);
+		}
+	}
+
+	//delete(T);
 	return newT;
 }
 
@@ -69,11 +142,7 @@ void setupB(std::vector<Real>& b) {//add your own parameters
 	}
 }
 
-void fillT() {//add your own parameters
-	// to be implemented
-	//fill T with solved vector x
-	//make sure that the temperature in boundary cells stays zero
-}
+
 
 void setupA(SparseMatrix<Real>& A, double factor) {//add your own parameters
 	// to be implemented
@@ -82,10 +151,16 @@ void setupA(SparseMatrix<Real>& A, double factor) {//add your own parameters
 	// if needed, read with: A(index1, index2);
 	// avoid zero rows in A -> set the diagonal value for boundary cells to 1.0
 	for (int i = 0; i < 25; i++) {
-			A.set_element(i, i, 1); // set diagonal
+		A.set_element(i, i, 1); // set diagonal
 	}
 }
 
+
+void fillT(std::vector<Real> x) {//add your own parameters
+	// to be implemented
+	//fill T with solved vector x
+	//make sure that the temperature in boundary cells stays zero
+}
 
 void DiffusionSimulator::diffuseTemperatureImplicit() {//add your own parameters
 	// solve A T = b
@@ -112,36 +187,35 @@ void DiffusionSimulator::diffuseTemperatureImplicit() {//add your own parameters
 	// preconditioners: 0 off, 1 diagonal, 2 incomplete cholesky
 	solver.solve(*A, *b, x, ret_pcg_residual, ret_pcg_iterations, 0);
 	// x contains the new temperature values
-	fillT();//copy x to T
+	fillT(x);//copy x to T
 }
-
-
-
-void DiffusionSimulator::simulateTimestep(float timeStep)
-{
-	// to be implemented
-	// update current setup for each frame
-	switch (m_iTestCase)
-	{
-	case 0:
-		T = diffuseTemperatureExplicit();
-		break;
-	case 1:
-		diffuseTemperatureImplicit();
-		break;
-	}
-}
-
-void DiffusionSimulator::drawObjects()
-{
-	// to be implemented
-	//visualization
-}
-
 
 void DiffusionSimulator::drawFrame(ID3D11DeviceContext* pd3dImmediateContext)
 {
 	drawObjects();
+}
+
+void DiffusionSimulator::drawObjects()
+{
+	const float sphereSize = .05f;
+
+	auto sizeX = T.GetSizeX();
+	auto sizeY = T.GetSizeY();
+	for (uint32_t y = 0; y < sizeY; y++)
+	{
+		for (uint32_t x = 0; x < sizeX; x++)
+		{
+			Vec3 pos(
+				((float)x - sizeX / 2) * sphereSize,
+				((float)y - sizeX / 2) * sphereSize,
+				0
+			);
+
+			auto tVal = T.GetValue(x, y);
+			DUC->setUpLighting(Vec3(), Vec3(tVal, 0, -tVal), 50., Vec3(tVal, 0, -tVal));
+			DUC->drawSphere(pos, Vec3(sphereSize * .5f, sphereSize * .5f, sphereSize * .5f));
+		}
+	}
 }
 
 void DiffusionSimulator::onClick(int x, int y)
--- Simulations/DiffusionSimulator.h
+++ Simulations/DiffusionSimulator.h
@@ -5,14 +5,29 @@
 #include "vectorbase.h"
 
 //impement your own grid class for saving grid data
+template <class T>
 class Grid {
 public:
 	// Construtors
-	Grid();
+	Grid(uint32_t x, uint32_t y)
+	{
+		this->_sizeX = x;
+		this->_sizeY = y;
 
+		_values.resize(x * y);
+	}
+
+	uint32_t GetSizeX() { return _sizeX; };
+	uint32_t GetSizeY() { return _sizeY; };
+
+	T GetValue(uint32_t x, uint32_t y) { return _values[_sizeX * y + x]; };
+	void SetValue(uint32_t x, uint32_t y, T val) { _values[_sizeX * y + x] = val; };
 
 private:
-	// Attributes
+	uint32_t _sizeX;
+	uint32_t _sizeY;
+
+	std::vector<T> _values;
 };
 
 
@@ -21,6 +36,7 @@ class DiffusionSimulator:public Simulator{
 public:
 	// Construtors
 	DiffusionSimulator();
+	~DiffusionSimulator();
 
 	// Functions
 	const char * getTestCasesStr();
@@ -34,10 +50,14 @@ public:
 	void onMouse(int x, int y);
 	// Specific Functions
 	void drawObjects();
-	Grid* diffuseTemperatureExplicit();
+	Grid<double> diffuseTemperatureExplicit(float timeStep);
 	void diffuseTemperatureImplicit();
 
 private:
+	// Functions
+	void ProcessInput();
+	void InitializeTemperatureGrid();
+
 	// Attributes
 	Vec3  m_vfMovableObjectPos;
 	Vec3  m_vfMovableObjectFinalPos;
@@ -45,7 +65,11 @@ private:
 	Point2D m_mouse;
 	Point2D m_trackmouse;
 	Point2D m_oldtrackmouse;
-	Grid *T; //save results of every time step
+
+	uint32_t _gridSizeX = 16;
+	uint32_t _gridSizeY = 16;
+	Grid<double> T; //save results of every time step
+	double _alpha = 1;
 };
 
 #endif
